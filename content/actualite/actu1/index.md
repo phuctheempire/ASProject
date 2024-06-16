85f0b41e34af4fd50a0c4793b1b085e164b2eb9e
Xuan Phuc PHAM <xuanphuc280203@gmail.com>
2024-06-16 09:48:21 +0200

    a

--- content/actualite/actu1/index.md
+++ content/actualite/actu1/index.md
@@ -4,3 +4,4 @@ date: 2020-01-01
 description: "Description de l'actualité 1"
 image: "image.jpg"
 ---
+ 
---
+++ pages/sport/all_sport.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="h-screen">
+        <Navbar/>
+        <div class="flex flex-col items-center justify-center">
+            <h1 class="text-4xl font-bold">All Sports</h1>
+            <div class="grid grid-cols-3 gap-4">
+                <div v-for="sport in sports" :key="sport.id" class="bg-gray-100 p-4 rounded-lg">
+                    <h2 class="text-xl font-bold">{{ sport.name }}</h2>
+                    <p>{{ sport.description }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
