2ad3ea6e41241e93327fca32bd3a9d26d4505ad7
agadae <146090492+agadae@users.noreply.github.com>
2024-06-17 14:33:08 +0200

    Update tennis.vue

--- pages/tennis.vue
+++ pages/tennis.vue
@@ -1,22 +1,44 @@
 <template>
   <div>
-  <div class="relative">
-    <nuxt-link to="/" class="absolute top-4 right-4 bg-blue-500 text-white px-4 py-2 rounded-md shadow-md">
-      Retour à l'accueil
-    </nuxt-link></div>  
+    <div class="relative">
+      <nuxt-link to="/" class="absolute top-4 right-4 bg-blue-500 text-white px-4 py-2 rounded-md shadow-md">
+        Retour à l'accueil
+      </nuxt-link>
+    </div>  
+
     <p>Bienvenue sur la page dédiée au tennis !</p>
-    <div class="info">
-      <h2>Informations</h2>
-      <p><strong>Responsable :</strong> Ludovick </p>
-      <p><strong>Jours et Horaires :</strong></p>
-      <ul>
-        <li>Jeudi : 15h30 - 17h00</li>
-      </ul>
+
+    <div>
+      <h1>Tennis - Bourges</h1>
+      <div class="info">
+        <p><strong>Responsables :</strong> Ludovick </p>
+        <p><strong>Jours et Horaires :</strong></p>
+        <ul>
+          <li>Jeudi : 15h30 - 17h00</li>
+        </ul>
+      </div>
+
+      <div class="location">
+        <h2>Lieu</h2>
+        <p>Bourges Tennis Club, 34 Boulevard de l'Industrie, 18000 Bourges</p>
+        <div id="map-bourges"></div>
+      </div>
     </div>
-    <div class="location">
-      <h2>Lieu</h2>
-      <p>Bourges Tennis Club, 34 Boulevard de l'Industrie, 18000 Bourges</p>
-      <div id="map"></div>
+
+    <div>
+      <h1>Tennis - Blois</h1>
+      <div class="info">
+        <p><strong>Responsable :</strong> machin</p>
+        <p><strong>Jours et Horaires :</strong></p>
+        <ul>
+          <li>Mardi : 17h00 - 18h30</li>
+        </ul>
+      </div>
+      <div class="location">
+        <h2>Lieu</h2>
+        <p>Gymnase Pierre de Coubertin, Rue des Quatre Vents, 41000 Blois</p>
+        <div id="map-blois"></div>
+      </div>
     </div>
   </div>
 </template>
@@ -28,19 +50,31 @@ import 'leaflet/dist/leaflet.css'
 export default {
   name: 'TennisPage',
   mounted() {
-    this.initMap()
+    this.initMapBourges();
+    this.initMapBlois();
   },
   methods: {
-    initMap() {
-      const map = L.map('map').setView([47.0727157, 2.3891492], 13)
+    initMapBourges() {
+      const mapBourges = L.map('map-bourges').setView([47.0727157, 2.3891492], 13);
 
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      }).addTo(map)
+      }).addTo(mapBourges);
 
-      L.marker([47.0727157, 2.3891492]).addTo(map) 
-        .bindPopup("Bourges Tennis Club<br>34 BOulevard de l Industrie, 18000 Bourges")
+      L.marker([47.0727157, 2.3891492]).addTo(mapBourges) 
+        .bindPopup("Bourges Tennis Club<br>34 Boulevard de l Industrie, 18000 Bourges")
         .openPopup()
+    },
+    initMapBlois() {
+      const mapBlois = L.map('map-blois').setView([47.5860922, 1.335947], 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(mapBlois);
+
+      L.marker([47.5860922, 1.335947]).addTo(mapBlois)
+        .bindPopup("Gymnase Pierre de Coubertin<br>Rue des Quatre Vents, 41000 Blois")
+        .openPopup();
     }
   }
 }
@@ -63,13 +97,18 @@ h1, h2 {
   color: #333;
 }
 
-p, ul {
-  font-size: 1.1em;
+h1 {
+  font-size : 1.3em;
+}
+
+p {
+  font-size: 1.5em;
 }
 
 ul {
   list-style-type: disc;
   margin-left: 20px;
+  font-size: 1.1em;
 }
 
 nav {
