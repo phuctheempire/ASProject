86f1814458c076ae9edae98148838a9679b35336
ibtissamBighrman <ibighrman@kali>
2024-06-19 11:54:04 +0200

    nouveausidebar

--- pages/test.vue
+++ pages/test.vue
@@ -1,22 +1,51 @@
 <template>
-    <div class="test-container">
-      <Sidebar />
+  <div class="test-container">
+    <Sidebar @toggle="handleSidebarToggle" />
+    <div :class="`main-content ${isSidebarOpen ? 'shifted' : ''}`">
+      <!-- Contenu principal de la page ici -->
+      <!-- Par exemple -->
+      <h1 class="welcome-message">Bienvenue sur le site de l'Association Sportive !</h1>
     </div>
-  </template>
-  
-  <script>
-  import Sidebar from '/components/SideBar.vue'; // Assurez-vous que le chemin est correct
-  
-  export default {
-    components: {
-      Sidebar
+  </div>
+</template>
+
+<script>
+import Sidebar from '/components/SideBar.vue'; 
+
+export default {
+  components: {
+    Sidebar
+  },
+  data() {
+    return {
+      isSidebarOpen: false,
+    };
+  },
+  methods: {
+    handleSidebarToggle(isOpen) {
+      this.isSidebarOpen = isOpen;
     }
   }
-  </script>
-  
-  <style>
-  .test-container {
-    position: relative;
-    height: 100vh; 
-  }
-  </style>  
+}
+</script>
+
+<style>
+.test-container {
+  position: relative;
+  height: 100vh;
+  transition: margin-left 0.3s;
+}
+
+.main-content {
+  transition: margin-left 0.3s;
+  margin-left: 0;
+}
+
+.main-content.shifted {
+  margin-left: 256px; 
+}
+
+.welcome-message {
+  padding: 20px;
+}
+</style>
